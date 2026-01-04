90c2996d7c29b98f449dbed9c28e075e33c458b2
clay-rgb <clay@tenju.ai>
2026-01-04 16:25:55 -0500

    Fix explore image mapping (swapped files to match titles)

--- frontend/src/content.js
+++ frontend/src/content.js
@@ -72,10 +72,10 @@ export const houseDetails = {
   location: "Castiglione Messer Raimondo, Italy",
   description: "Newly renovated home in hills of Abruzzo close to the picturesque medieval town of Appignano di Teramo. Beautiful views over the hilltowns with access to the sea and mountains.",
   specs: {
-    guests: 4,
-    bedrooms: 2,
-    beds: 2,
-    baths: 3,
+    guests: 8,
+    bedrooms: 4,
+    beds: 4,
+    baths: 4,
   },
   amenities: [
     "Wifi",
